2e3c813e972542f424d2e719134b9bfa34a1a897
Michelle Poterek and jacob ruleaux <michelle+jacob@>
2017-07-17 11:12:49 -0700

    adjust twig routes

--- app/app.php
+++ app/app.php
@@ -18,34 +18,56 @@
     ));
 
     $app->get("/", function() use ($app) {
-        return $app['twig']->render('index.html.twig', array(Category::getAll()));
+    return $app['twig']->render('index.html.twig', array('categories' => Category::getAll(), 'tasks' => Task::getAll()));
+    });
+
+
+    $app->get("/categories", function() use ($app) {
+        return $app['twig']->render('categories.html.twig', array(Category::getAll()));
     });
 
     $app->get("/tasks", function() use ($app) {
-        return $app['twig']->render('category.html.twig', array('tasks' => Task::getAll()));
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
+
+    // $app->post("/tasks", function() use ($app) {
+    //     $description = $_POST['description'];
+    //     $category_id = $_POST['category_id'];
+    //     $due_date = $_POST['due-date'];
+    //     $task = new Task($description, $category_id, $due_date, $id = null);
+    //     $task->save();
+    //     $category = Category::find($category_id);
+    //     // var_dump($category->getTasks());
+    //     return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks(), 'due-date' => $due_date));
+    // });
 
     $app->post("/tasks", function() use ($app) {
         $description = $_POST['description'];
-        $category_id = $_POST['category_id'];
-        $due_date = $_POST['due-date'];
-        $task = new Task($description, $category_id, $due_date, $id = null);
+        $task = new Task($description);
         $task->save();
-        $category = Category::find($category_id);
-        // var_dump($category->getTasks());
-        return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks(), 'due-date' => $due_date));
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
 
+    $app->get("/tasks/{id}", function($id) use ($app) {
+        $task = Task::find($id);
+        return $app['twig']->render('task.html.twig', array('task' => $task, 'categories' => $task->getCategories(), 'all_categories' => Category::getAll()));
+    });
+
+
+    // $app->get("/categories/{id}", function($id) use ($app) {
+    //     $category = Category::find($id);
+    //     return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks()));
+    // });
     $app->get("/categories/{id}", function($id) use ($app) {
         $category = Category::find($id);
-        return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks()));
+        return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks(), 'all_tasks' => Task::getAll()));
     });
 
     $app->post("/categories", function() use ($app) {
-        $category = new Category($_POST['category']);
-        $category->save();
-        return $app['twig']->render('index.html.twig', array('categories' => Category::getAll()));
-    });
+      $category = new Category($_POST['name']);
+      $category->save();
+      return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+  });
 
     $app->post("/delete_tasks", function() use ($app) {
         Task::deleteAll();
